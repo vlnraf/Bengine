f72aeb8848cb06911d342acc0b7d5b5a3a7819e0
Raffaele <raffaele.villani17@gmail.com>
2023-02-21 13:23:27 +0100

    simple game engine class that say hello world

--- src/GameEngine.cpp
+++ src/GameEngine.cpp
@@ -1,6 +1,18 @@
 #include "GameEngine.hpp"
+#include <iostream>
+#include <SDL2/SDL.h>
 
-int main (int argv, char *args[]){
-    std::cout << "ciao" << std::endl;
-    return 0;
+GameEngine::GameEngine(){}
+GameEngine::~GameEngine(){}
+
+void GameEngine::initialize(std::string p){
+    message = p;
+};
+
+void GameEngine::run(){
+    std::cout << message << std::endl;
+    while(count < 10){
+        std::cout << count << std::endl;
+        count++;
+    }
 }
--- src/GameEngine.hpp
+++ src/GameEngine.hpp
@@ -1,22 +1,14 @@
 #ifndef GAMEENGINE_H
 #define GAMEENGINE_H
 #include <iostream>
-#include <SDL2/SDL.h>
 
 class GameEngine{
     public:
         GameEngine();
         ~GameEngine();
 
-        void initialize(std::string p){
-            message = p;
-        };
-        void run(){
-            std::cout << message << std::endl;
-            while(true){
-                std::cout << count << std::endl;
-            }
-        };
+        void initialize(std::string p);
+        void run();
 
     private:
     int count = 0;
