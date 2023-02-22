babf1aa2a64b15f2d5767c760bb1a8dfcf01d1e7
Raffaele <raffaele.villani17@gmail.com>
2023-02-22 08:59:12 +0100

    texture manager to load textures

--- src/TextureManager.cpp
+++ src/TextureManager.cpp
@@ -1,11 +1,16 @@
 #include "TextureManager.hpp"
+#include <SDL2/SDL_image.h>
 
-SDL_Texture* TextureManager::rectTexture(SDL_Renderer *renderer, SDL_Rect rect, SDL_Color c){
-    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 2000, 2000);
-    SDL_SetRenderTarget(renderer, texture);
-    SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
-    SDL_RenderClear(renderer);
-    SDL_SetRenderTarget(renderer, NULL);
+SDL_Texture* TextureManager::LoadTexture(SDL_Renderer *renderer, std::string objectTexture){
+    //SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 1000, 1000);
+    //SDL_SetRenderTarget(renderer, texture);
+    //SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a);
+    //SDL_RenderClear(renderer);
+    //SDL_SetRenderTarget(renderer, NULL);
+
+    SDL_Surface *surface = IMG_Load(objectTexture.c_str());
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);
     
     return texture;
 };
--- src/TextureManager.hpp
+++ src/TextureManager.hpp
@@ -1,10 +1,27 @@
 #ifndef TEXTUREMANAGER_H
 #define TEXTUREMANAGER_H
-#include "GameEngine.hpp"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define _COLOR(RED, GREEN, BLUE)    { RED, GREEN, BLUE, 0xFF }
+#define COLOR(RED, GREEN, BLUE)     ((SDL_Color) { RED, GREEN, BLUE, 0xFF })
+
+const SDL_Color BLACK         = _COLOR(0, 0, 0);
+const SDL_Color WHITE         = _COLOR(0xFF, 0xFF, 0xFF);
+const SDL_Color GRAY          = _COLOR(0x64, 0x64, 0x64);
+const SDL_Color DARK_GRAY     = _COLOR(0x1E, 0x1E, 0x1E);
+const SDL_Color LIGHT_GRAY    = _COLOR(0xC8, 0xC8, 0xC8);
+const SDL_Color RED           = _COLOR(0xF5, 0x3B, 0x56);
+const SDL_Color GREEN         = _COLOR(0x01, 0x9F, 0x13);
+const SDL_Color BLUE          = _COLOR(0x38, 0x95, 0xD3);
+const SDL_Color YELLOW        = _COLOR(0xF7, 0xDC, 0x11);
+const SDL_Color ORANGE        = _COLOR(0xFF, 0x85, 0);
+const SDL_Color PINK          = _COLOR(0xFF, 0, 0xCE);
+const SDL_Color VIOLET        = _COLOR(0x91, 0, 0xFF);
 
 class TextureManager{
     public:
-        static SDL_Texture* rectTexture(SDL_Renderer *renderer, SDL_Rect rect, SDL_Color c);
+        static SDL_Texture* LoadTexture(SDL_Renderer *renderer, std::string texture);
 };
 
 
